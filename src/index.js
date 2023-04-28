6ad015d57fc2904ea8c23add4215d61383e23023
jun-yunan <junyunan341695@gmail.com>
2023-04-28 19:01:11 +0700

    Course detail page

--- src/index.js
+++ src/index.js
@@ -29,12 +29,10 @@ app.engine('hbs', handlebars({
     extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources//views'));
+app.set('views', path.join(__dirname, 'resources','views'));
 
 // route init
 route(app)
 
 
-
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
