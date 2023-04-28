b6127de8010dfcf1d1e68c12af6513d909480d7c
jun-yunan <junyunan341695@gmail.com>
2023-04-28 12:14:48 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -19,12 +26,10 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources//views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+// route init
+route(app)
 
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
