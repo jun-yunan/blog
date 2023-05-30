b354d3aebef4137e117574f7d08e997e1333ed38
jun-yunan <junyunan341695@gmail.com>
2023-05-30 22:48:00 +0700

    deploy #7

--- src/database/index.js
+++ src/database/index.js
@@ -6,10 +6,10 @@ async function connect() {
         // await mongoose.connect(`${process.env.DB}${databaseConfig.dbName}`, {
         const connect = await mongoose.connect(
             `mongodb+srv://jun-yunan:${process.env.PASSWORD}@cluster0.vtrs6li.mongodb.net/WebsiteSales?retryWrites=true&w=majority`,
-            // {
-            //     useNewUrlParser: true,
-            //     useUnifiedTopology: true,
-            // },
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
         );
         console.log(`Connect database WebsiteSales successfully!!!`);
     } catch (error) {
