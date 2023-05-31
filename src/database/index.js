a1920db522766601a1ce12adc1e36f7c7d85da3a
jun-yunan <junyunan341695@gmail.com>
2023-05-31 15:41:49 +0700

    deploy #15

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 async function connect() {
     try {
         const connect = await mongoose.connect(
-            // `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.vtrs6li.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`,
-            `mongodb+srv://jun-yunan:0794210593Zed@cluster0.jy85qob.mongodb.net/WebsiteSales?retryWrites=true&w=majority`,
+            `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.jy85qob.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
